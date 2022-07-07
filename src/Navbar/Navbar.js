d56d09270672bee5cb75d8866ab290114acc9dbb
Nicolassaint <71348369+Nicolassaint@users.noreply.github.com>
2022-07-07 15:10:32 +0200

    test

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React,{useState,useEffect} from 'react'
+
+export default function 
+
+() {
+  return (
+    <nav>
+        <ul className='liste'>
+            <li className='items'>Accueil</li>
+            <li className='items'>Services</li>
+            <li className='items'>Contact</li>
+
+        </ul>
+
+        <button className='btn'>BTN</button>
+
+    </nav>
+  )
+}
